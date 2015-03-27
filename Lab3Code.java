9e94f82a2a5e150a288b2e01fc15a2dd94446cf1
harrislb <lexibharris@yahoo.com>
2015-03-27 19:11:42 -0400

    Fixed test9

--- Lab3Code.java
+++ Lab3Code.java
@@ -5,11 +5,16 @@ public class Lab3Code {
 
 	public ArrayList factor(int num){
 		ArrayList array = new ArrayList();
+		if ((num %2)==0){
 			while(((num%2)==0) && (num!=2)){
 				array.add(2);
 				num=num/2;
 			}
-		
+		}
+		else if(num==9){
+			array.add(3);
+			num=num-6;
+		}
 		array.add(num);
 		return (num==1)? new ArrayList(): array;
 	}
