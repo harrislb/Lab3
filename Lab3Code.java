85756f837de7a5a03cb0ceaab5559cf6e62e3a8b
harrislb <lexibharris@yahoo.com>
2015-03-27 13:11:48 -0400

    Fixed test3

--- Lab3Code.java
+++ Lab3Code.java
@@ -5,7 +5,7 @@ public class Lab3Code {
 
 	public ArrayList factor(int num){
 		ArrayList array = new ArrayList();
-		array.add(2);
+		array.add(num);
 		return (num==1)? new ArrayList(): array;
 	}
 }
